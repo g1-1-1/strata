f77e4ac1a52a156268d1b322b8f8a3f2c81c3fc5
jupiter <syl@waifu.club>
2023-01-01 18:39:35 +1100

    fix: casing

--- js/lz-string.js
+++ js/lz-string.js
@@ -1,19 +1,19 @@
 const LZString = {
-  // Converts an ASCII code to a character
+  // converts an ASCII code to a character
   fromCharCode: String.fromCharCode,
 
-  // Base64 encoding key string
+  // base64 encoding key string
   base64KeyString: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
   // URI-safe encoding key string
   uriSafeKeyString: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",
 
-  // Reverse dictionary for base encoding
+  // reverse dictionary for base encoding
   baseReverseDictionary: {},
 
-  // Returns the base value for the given base and character
+  // returns the base value for the given base and character
   getBaseValue(base, character) {
-    // Create reverse dictionary if it doesn't exist
+    // create reverse dictionary if it doesn't exist
     if (!this.baseReverseDictionary[base]) {
       this.baseReverseDictionary[base] = {};
       for (let i = 0; i < base.length; i++) {
@@ -21,21 +21,21 @@ const LZString = {
       }
     }
 
-    // Return the base value for the given character
+    // return the base value for the given character
     return this.baseReverseDictionary[base][character];
   },
 
-  // Compresses the given string to a base64-encoded string
+  // compresses the given string to a base64-encoded string
   compressToBase64(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Compress the string and convert to base64
+    // compress the string and convert to base64
     const compressed = this._compress(string, 6, function(code) {
       return this.base64KeyString.charAt(code);
     });
 
-    // Add padding to the compressed string if necessary
+    // add padding to the compressed string if necessary
     switch (compressed.length % 4) {
       case 0:
         return compressed;
@@ -48,12 +48,12 @@ const LZString = {
     }
   },
 
-  // Decompresses a base64-encoded string to the original string
+  // decompresses a base64-encoded string to the original string
   decompressFromBase64(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Return null if the input is an empty string
+    // return null if the input is an empty string
     if (string === "") return null;
 
     // Decompress the string and convert from base64
@@ -62,33 +62,33 @@ const LZString = {
     });
   },
 
-  // Compresses the given string to a UTF-16 encoded string
+  // compresses the given string to a UTF-16 encoded string
   compressToUTF16(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Compress the string and convert to UTF-16
+    // compress the string and convert to UTF-16
     return this._compress(string, 15, function(code) {
       return String.fromCharCode(code + 32);
     }) + " ";
   },
 
-  // Decompresses a UTF-16 encoded string to the original string
+  // decompresses a UTF-16 encoded string to the original string
   decompressFromUTF16(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Return null if the input is an empty string
+    // return null if the input is an empty string
     if (string === "") return null;
 
-    // Decompress the string and convert from UTF-16
+    // decompress the string and convert from UTF-16
     return this._decompress(string.length, 16384, function(index) {
       return string.charCodeAt(index) - 32;
     });
   },
 
-  // Compresses the given string to a Uint8Array
-  compressToUint8Array(string) {    // Compress the string and convert to a Uint8Array
+  // compresses the given string to a Uint8Array
+  compressToUint8Array(string) {    // compress the string and convert to a Uint8Array
     const compressed = this.compress(string);
     const uint8Array = new Uint8Array(2 * compressed.length);
     for (let i = 0; i < compressed.length; i++) {
@@ -99,12 +99,12 @@ const LZString = {
     return uint8Array;
   },
 
-  // Decompresses a Uint8Array to the original string
+  // decompresses a Uint8Array to the original string
   decompressFromUint8Array(uint8Array) {
-    // Return null if the input is null or undefined
+    // return null if the input is null or undefined
     if (uint8Array == null) return null;
 
-    // Convert the Uint8Array to a string
+    // convert the Uint8Array to a string
     const array = [];
     for (let i = 0; i < uint8Array.length; i++) {
       array.push(uint8Array[i]);
@@ -113,27 +113,27 @@ const LZString = {
       return String.fromCharCode(code);
     }).join("");
 
-    // Decompress the string
+    // decompress the string
     return this.decompress(string);
   },
 
-  // Compresses the given string to an encoded URI component
+  // compresses the given string to an encoded URI component
   compressToEncodedURIComponent(string) {
     // Return an empty string if the input is null
     if (string == null) return "";
 
-    // Compress the string and convert to an encoded URI component
+    // compress the string and convert to an encoded URI component
     return this._compress(string, 6, function(code) {
       return this.uriSafeKeyString.charAt(code);
     });
   },
 
-  // Decompresses an encoded URI component to the original string
+  // decompresses an encoded URI component to the original string
   decompressFromEncodedURIComponent(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Return null if the input is an empty string
+    // return null if the input is an empty string
     if (string === "") return null;
 
     // Decompress the string and convert from an encoded URI component
@@ -142,20 +142,20 @@ const LZString = {
     });
   },
 
-  // Compresses the given string
+  // compresses the given string
   compress(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Compress the string and return the result
+    // compress the string and return the result
     return this._compress(string, 16, function(code) {
       return String.fromCharCode(code);
     });
   },
 
-  // Private function for compressing a string
+  // private function for compressing a string
   _compress(string, bitsPerChar, getCharFromInt) {
-    // Initialize variables for the loop
+    // initialize variables for the loop
     let value = 0;
     let position = 0;
     let dictionary = {};
@@ -163,50 +163,50 @@ const LZString = {
     let w;
     let c;
 
-    // Loop through each character in the string
+    // loop through each character in the string
     for (let i = 0; i < string.length; i++) {
-      // Get the character at the current position
+      // get the character at the current position
       c = string[i];
 
-      // Check if the character is in the dictionary
+      // check if the character is in the dictionary
       if (dictionary[c] !== undefined) {
-        // Set the word to the character
+        // set the word to the character
         w = c;
       } else {
-        // Set the word to the current character and the next character
+        // set the word to the current character and the next character
         w = c + string[i + 1];
       }
 
-      // Check if the word is in the dictionary
+      // check if the word is in the dictionary
       if (dictionary[w] !== undefined) {
-        // Update the value and position
+        // update the value and position
         value = (value << bitsPerChar) + dictionary[w];
         position += bitsPerChar;
       } else {
-        // Add the current value to the result
+        // add the current value to the result
         result += getCharFromInt(value >> position);
         position = position - bitsPerChar;
 
-        // Add the new word to the dictionary
+        // add the new word to the dictionary
         dictionary[w] = bitsPerChar;
         value = (value << bitsPerChar) + bitsPerChar;
         position += bitsPerChar;
       }
     }
 
-    // Add the remaining value to the result
+    // add the remaining value to the result
     result += getCharFromInt(value >> position);
 
-    // Return the result
+    // return the result
     return result;
   },
 
-  // Decompresses the given string
+  // decompresses the given string
   decompress(string) {
-    // Return an empty string if the input is null
+    // return an empty string if the input is null
     if (string == null) return "";
 
-    // Initialize variables for the loop
+    // initialize variables for the loop
     let dictionary = [];
     let nextChar = "";
     let word = "";
@@ -215,52 +215,52 @@ const LZString = {
     let value = 0;
     let position = 0;
 
-    // Loop through each character in the string
+    // loop through each character in the string
     for (let i = 0; i < string.length; i++) {
-      // Get the character at the current position
+      // get the character at the current position
       c = string[i];
 
-      // Check if the character is a number
+      // check if the character is a number
       if (+c === c) {
-        // Update the value and position
+        // update the value and position
         value = (value << bitsPerChar) + c;
         position += bitsPerChar;
-      } else       // Update the dictionary with the new entry
+      } else       // update the dictionary with the new entry
       dictionary.push(entry);
 
-      // Set the next character to the entry
+      // set the next character to the entry
       nextChar = entry;
 
-      // Initialize the result to the next character
+      // initialize the result to the next character
       result = nextChar;
 
-      // Loop through the rest of the string
+      // loop through the rest of the string
       for (; i < string.length; i++) {
-        // Get the character at the current position
+        // get the character at the current position
         c = string[i];
 
-        // Check if the character is a number
+        // check if the character is a number
         if (+c === c) {
-          // Update the value and position
+          // update the value and position
           value = (value << bitsPerChar) + c;
           position += bitsPerChar;
         } else {
-          // Check if the value is within the dictionary range
+          // check if the value is within the dictionary range
           if (value in dictionary) {
-            // Set the word to the value in the dictionary
+            // set the word to the value in the dictionary
             word = dictionary[value];
           } else {
-            // Set the word to the next character and the first character of the current entry
+            // set the word to the next character and the first character of the current entry
             word = nextChar + entry[0];
           }
 
-          // Update the result with the word
+          // update the result with the word
           result += word;
 
-          // Update the dictionary with the new entry
+          // update the dictionary with the new entry
           dictionary.push(word);
 
-          // Set the next character to the first character of the current entry
+          // set the next character to the first character of the current entry
           nextChar = entry;
         }
       }
